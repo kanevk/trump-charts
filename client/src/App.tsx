4087fc107bb4990f5964bd6af4219dfcb23c414b
kanevk <kamen.e.kanev@gmail.com>
2020-10-22 09:46:46 +0300

    Implement inital version of the charts
     - use fake graphql backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,87 @@
-import React from 'react';
-import './App.scss';
+import "./App.scss";
+import React from "react";
+import { ResponsiveContainer, PieChart, Pie, Cell } from "recharts";
+import { DocumentNode, gql, useQuery } from "@apollo/client";
 
-function App() {
+const QUERY: DocumentNode = gql`
+  query contriesOccurrences {
+    tweetsAnalytics {
+      contriesOccurrences {
+        name
+        occurrencesCount
+      }
+    }
+  }
+`;
+
+const ColumnChart = ({
+  title,
+  data,
+}: {
+  title: string;
+  data: readonly object[];
+}) => {
+  const colors: string[] = [
+    '#ffe200',
+    '#ff7105'
+  ]
+
   return (
-    <div className="App">
+    <div className="column">
+      <ResponsiveContainer height={250}>
+        <PieChart>
+          <Pie
+            data={data}
+            dataKey="value"
+            nameKey="name"
+            cx="50%"
+            cy="50%"
+            innerRadius={60}
+            outerRadius={80}
+            label
+          >
+            {data.map((_entry, index) => (
+              <Cell key={`cell-${index}`} fill={colors[index]} />
+            ))}
+          </Pie>
+        </PieChart>
+      </ResponsiveContainer>
+      <span className="title">{title}</span>
     </div>
   );
-}
+};
+
+const App = () => {
+  const { data, loading, error } = useQuery(QUERY);
+
+  if (loading) return <div></div>;
+  if (error) throw error;
+
+  const chartData = data.tweetsAnalytics.contriesOccurrences.map(
+    ({
+      name,
+      occurrencesCount,
+    }: {
+      name: string;
+      occurrencesCount: string;
+    }) => ({ name, value: occurrencesCount }),
+  );
+
+  return (
+    <div className="App">
+      <div className="main">
+        <header>
+          <img src="favicon.ico" alt="Thrub's face"></img>
+          <h2>Thrumb's charts</h2>
+        </header>
+        <div className="columns">
+          <ColumnChart title="US vs Russia" data={chartData} />
+          <ColumnChart title="Favourite child" data={chartData} />
+          <ColumnChart title="Occurrencies of 'democracy' till date" data={chartData} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:9002/graphql',
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
